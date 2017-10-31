09622272ecb53cb6c915fe6e7f742d322b3f7d37
abinator-1308 <abinator.as@gmail.com>
2017-10-31 20:06:14 +0530

    Update Ide (4).cpp
    BITMASKING IMPLEMENTED

--- Ide (4).cpp
+++ Ide (4).cpp
@@ -1,10 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int func(int mask,int idx)
+{
+	if(idx==101)
+	{
+		if(mask==((1<<n)-1))
+			return 1;
+		return 0;
+	}
+	int &ans=dp[mask][idx];
+	if(ans!=-1)
+		return ans;
+	ans=0;
+	int i;
+	fl(i,v1[idx].size())
+	{
+		int p=v1[idx][i];
+		if((mask&(1<<p))==0)
+			ans+=func(mask|(1<<p),idx+1);
+	}
+	return ans;
+}
+
 int main() {
 	// your code goes here
 	int t,n,i,j,a[202],x1,x2,y1,y2;
 	//char ;
+	
 	scanf("%d",&t);
 	while(t--)
 	{
